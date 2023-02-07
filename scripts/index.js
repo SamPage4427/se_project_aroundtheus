48bfb837766716633d49a5b86d7ae67f4c05b5f7
SamPage4427 <sampage4427@gmail.com>
2023-02-07 14:29:34 -0700

    Added JavaScript objects in an array

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+let initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: URL(
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+    ),
+  },
+  {
+    name: "Lake Louise",
+    link: URL(
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
+    ),
+  },
+  {
+    name: "Bald Mountains",
+    link: URL(
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
+    ),
+  },
+  {
+    name: "Latemar",
+    link: URL(
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+    ),
+  },
+  {
+    name: "Vanoise National Park",
+    link: URL(
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+    ),
+  },
+  {
+    name: "Lago di Braies",
+    link: URL(
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
+    ),
+  },
+];
