867250dae797f42059977f0e66fc1a37bed89213
SamPage4427 <sampage4427@gmail.com>
2023-02-13 13:00:59 -0700

    Updated cursor style for buttons and added modal functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,40 @@
 let initialCards = [
   {
     name: "Yosemite Valley",
-    link: URL(
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-    ),
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
   {
     name: "Lake Louise",
-    link: URL(
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
-    ),
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
   {
     name: "Bald Mountains",
-    link: URL(
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
-    ),
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
   {
     name: "Latemar",
-    link: URL(
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
-    ),
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
   {
     name: "Vanoise National Park",
-    link: URL(
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
-    ),
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
   {
     name: "Lago di Braies",
-    link: URL(
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
-    ),
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditBtn = document.querySelector(".profile__edit");
+const profileEditModal = document.querySelector(".modal");
+const profileEditClose = document.querySelector(".modal__close");
+
+profileEditBtn.addEventListener("click", () => {
+  console.log("button clicked");
+  profileEditModal.classList.add("modal__opened");
+});
+
+profileEditClose.addEventListener("click", () => {
+  console.log("closed");
+  profileEditModal.classList.remove("modal__opened");
+});
