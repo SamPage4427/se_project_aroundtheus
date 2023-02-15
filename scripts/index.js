52804da9697b5d05e92568d63f0940479bf9cd60
SamPage4427 <sampage4427@gmail.com>
2023-02-14 20:32:21 -0700

    feat: added ability to change profile name and description, as well as form submission and card functionality

--- scripts/index.js
+++ scripts/index.js
@@ -28,13 +28,48 @@ let initialCards = [
 const profileEditBtn = document.querySelector(".profile__edit");
 const profileEditModal = document.querySelector(".modal");
 const profileEditClose = document.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const modalTitle = document.querySelector("#profile-title-input");
+const modalDescription = document.querySelector("#profile-title-description");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListElement = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
-profileEditBtn.addEventListener("click", () => {
-  console.log("button clicked");
-  profileEditModal.classList.add("modal__opened");
-});
-
-profileEditClose.addEventListener("click", () => {
-  console.log("closed");
+function closePopUp() {
   profileEditModal.classList.remove("modal__opened");
-});
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = modalTitle.value;
+  profileDescription.textContent = modalDescription.value;
+  closePopUp();
+}
+
+function profileModalOpen() {
+  modalTitle.value = profileTitle.textContent;
+  modalDescription.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal__opened");
+}
+
+profileEditBtn.addEventListener("click", profileModalOpen);
+
+profileEditClose.addEventListener("click", closePopUp);
+
+profileEditForm.addEventListener("submit", handleFormSubmit);
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImgElement = cardElement.querySelector(".card__image");
+  const cardTitleElement = cardElement.querySelector(".card__title");
+  cardImgElement.src = data.link;
+  cardImgElement.alt = data.name;
+  cardTitleElement.textContent = data.name;
+  return cardElement;
+}
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardListElement.append(cardElement);
+}
